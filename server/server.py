26c405b9ce0903118aa53120a3568359669792d8
De Long Fu <delongfu@gmail.com>
2013-11-02 16:01:47 -0700

    server error handling and book lookup

--- server/server.py
+++ server/server.py
@@ -10,16 +10,19 @@ db = db_client["socialpub"]
 
 app = Flask(__name__)
 
-def booktableLookup(bookid):
+def bookTableLookup(bookid):
     return db["books"].find_one({"bookid":bookid})
 
 @app.route("/getcomments", methods=["GET"])
 def getcomments():
-    bookid = request.args.get("bookid", "")
-    userid = request.args.get("userid", "")
-    print "getting comment for user " + userid
+    bookid = int(request.args.get("bookid", 1))
+    page = int(request.args.get("page", 0))
+    collection = bookTableLookup(bookid)
+    if not collection:
+        return dumps({"error": "no such book"})
+    collection = collection["collection"]
 
-    results = [x for x in db["testbook"].find({"userid": int(userid)})]
+    results = [x for x in db[collection].find({"page":page})]
     return dumps(results)
 
 
