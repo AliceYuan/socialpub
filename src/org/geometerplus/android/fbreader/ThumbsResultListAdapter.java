364580af0152781cb93a95689cd4cf1ad9e1ca27
Alice Yuan <me@aliceyuan.ca>
2013-11-03 02:20:12 -0800

    made things prettier!

--- src/org/geometerplus/android/fbreader/ThumbsResultListAdapter.java
+++ src/org/geometerplus/android/fbreader/ThumbsResultListAdapter.java
@@ -6,7 +6,6 @@ import org.geometerplus.zlibrary.ui.android.R;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,13 +17,13 @@ import android.widget.TextView;
 import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
 
 public class ThumbsResultListAdapter extends ArrayAdapter<ResultObject> {
-	private Context context; 
-	private int layoutResourceId;    
+	private Context context;
+	private int layoutResourceId;
 	private ArrayList<ResultObject> data = null;
 	private resultHolder holder = null;
 
-
-	public ThumbsResultListAdapter(FragmentActivity fragmentActivity, int layoutResourceId,	ArrayList<ResultObject> listData) {
+	public ThumbsResultListAdapter(FragmentActivity fragmentActivity,
+			int layoutResourceId, ArrayList<ResultObject> listData) {
 		super(fragmentActivity, layoutResourceId, listData);
 		this.layoutResourceId = layoutResourceId;
 		this.context = fragmentActivity;
@@ -35,37 +34,37 @@ public class ThumbsResultListAdapter extends ArrayAdapter<ResultObject> {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View row = convertView;
 
-		if(row == null)
-		{
-			LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+		if (row == null) {
+			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
 			row = inflater.inflate(layoutResourceId, parent, false);
 
 			holder = new resultHolder();
-			holder.profileIcon = (ImageView)row.findViewById(R.id.img_thumbnail);
-			holder.commentText = (TextView) row.findViewById(R.id.search_field);
+			holder.profileIcon = (RoundedImageView) row
+					.findViewById(R.id.img_thumbnail);
+			holder.commentText = (TextView) row
+					.findViewById(R.id.comment_text_field);
 			row.setTag(holder);
 		} else {
-			holder = (resultHolder)row.getTag();
+			holder = (resultHolder) row.getTag();
 			holder.profileIcon.setImageDrawable(null);
 		}
 
 		final ResultObject result = data.get(position);
 		holder.commentText.setText(result.getTitle());
-		DiscussionFragment.imageLoader.displayImage(result.getImageUrl(),holder.profileIcon, new SimpleImageLoadingListener() {
-			@Override
-			public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-				holder.profileIcon.setImageBitmap(loadedImage);
-			}
-		});
-
+		DiscussionFragment.imageLoader.displayImage(result.getImageUrl(),
+				holder.profileIcon, new SimpleImageLoadingListener() {
+//					@Override
+//					public void onLoadingComplete(String imageUri, View view,
+//							Bitmap loadedImage) {
+//					}
+				});
 
 		return row;
 	}
 
-	//photo result holder of views
-	private class resultHolder
-	{
-		ImageView profileIcon;
-		TextView  commentText;
+	// photo result holder of views
+	private class resultHolder {
+		RoundedImageView profileIcon;
+		TextView commentText;
 	}
 }
