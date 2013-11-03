ab362fcc08725fbad6b02255078b29355afaaad2
Alice Yuan <me@aliceyuan.ca>
2013-11-03 05:54:31 -0800

    comment box

--- src/org/geometerplus/android/fbreader/DiscussionFragment.java
+++ src/org/geometerplus/android/fbreader/DiscussionFragment.java
@@ -1,51 +1,66 @@
 package org.geometerplus.android.fbreader;
 
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import org.geometerplus.zlibrary.ui.android.R;
 
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 public class DiscussionFragment extends Fragment {
-    /** (non-Javadoc)
-     * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
-     */
+	/** (non-Javadoc)
+	 * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	 */
 	private ThumbsResultListAdapter resultAdapter;
 	private ListView listDisplay;
 	public static ImageLoader imageLoader = ImageLoader.getInstance();
 	public static ImageLoaderConfiguration config;
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        if (container == null) {
-            // We have different layouts, and in one of them this
-            // fragment's containing frame doesn't exist.  The fragment
-            // may still be created from its saved state, but there is
-            // no reason to try to create its view hierarchy because it
-            // won't be displayed.  Note this is not needed -- we could
-            // just run the code below, where we would create and return
-            // the view hierarchy; it would just never be used.
-            return null;
-        }
-        return (LinearLayout)inflater.inflate(R.layout.tabs_fragment_discussion, container, false);
-    }
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		if (container == null) {
+			// We have different layouts, and in one of them this
+			// fragment's containing frame doesn't exist.  The fragment
+			// may still be created from its saved state, but there is
+			// no reason to try to create its view hierarchy because it
+			// won't be displayed.  Note this is not needed -- we could
+			// just run the code below, where we would create and return
+			// the view hierarchy; it would just never be used.
+			return null;
+		}
+		return (RelativeLayout)inflater.inflate(R.layout.tabs_fragment_discussion, container, false);
+	}
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
-        listDisplay = (ListView) getView().findViewById(R.id.list_view);
+		listDisplay = (ListView) getView().findViewById(R.id.list_view);
 
-        resultAdapter = new ThumbsResultListAdapter(getActivity(), R.layout.listview_item_thumb_result, ReSoViewPagerFragmentActivity.getListData());
-        listDisplay.setAdapter(resultAdapter);
+		resultAdapter = new ThumbsResultListAdapter(getActivity(), R.layout.listview_item_thumb_result, ReSoViewPagerFragmentActivity.getListData());
+		listDisplay.setAdapter(resultAdapter);
+		ReSoViewPagerFragmentActivity.commentText = (EditText)getActivity().findViewById(R.id.comment_edit_text);
+
+		ReSoViewPagerFragmentActivity.commentText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus)
+					ReSoViewPagerFragmentActivity.commentText.setText("");
+			}
+		});
+
 	}
-    
-    
+
 }
--- src/org/geometerplus/android/fbreader/ReSoViewPagerFragmentActivity.java
+++ src/org/geometerplus/android/fbreader/ReSoViewPagerFragmentActivity.java
@@ -1,26 +1,32 @@
 package org.geometerplus.android.fbreader;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Vector;
 
 import org.geometerplus.zlibrary.ui.android.R;
 
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 public class ReSoViewPagerFragmentActivity extends FragmentActivity{
 	/** maintains the pager adapter*/
 	private PagerAdapter mPagerAdapter;
 	private ImageLoaderConfiguration config;
 	private static ArrayList<ResultObject> listData = new ArrayList<ResultObject>();
-
+	public static EditText commentText;
 	/* (non-Javadoc)
 	 * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
 	 */
@@ -47,9 +53,32 @@ public class ReSoViewPagerFragmentActivity extends FragmentActivity{
 		listData.add(new ResultObject("alice","alice","alice","alice","alice","http://farm5.staticflickr.com/4121/4795424879_7e35b2cbf4_s.jpg"));
 		this.mPagerAdapter  = new ReSoPagerAdapter(super.getSupportFragmentManager(), fragments);
 		//
+		
 		ViewPager pager = (ViewPager)super.findViewById(R.id.viewpager);
 		pager.setAdapter(this.mPagerAdapter);
 	}
+
+	
+	public void sendComment(View view){
+		//get user entered search term
+		String comment = commentText.getText().toString();
+		if(comment.length()>0){
+			try{
+				//instantiate and execute AsyncTask
+				InputMethodManager in = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+				in.hideSoftInputFromWindow(commentText.getWindowToken(), 0);
+				Calendar c = Calendar.getInstance();
+		        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		        String formattedDate = df.format(c.getTime());
+				new ResultObject(comment, "1234", "1234", "1234", formattedDate, "blah");
+			}
+			catch(Exception e){ 
+				e.printStackTrace(); 
+			}
+		}
+	}
+	
+	
 
 
 	public static ArrayList<ResultObject> getListData() {
