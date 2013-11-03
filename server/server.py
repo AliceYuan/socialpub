580108ca2c9c2bd2ab5464387721560d0e75ac43
De Long Fu <delongfu@gmail.com>
2013-11-03 01:45:04 -0800

    server: fuck this shit

--- server/server.py
+++ server/server.py
@@ -10,37 +10,38 @@ db = db_client["socialpub"]
 
 app = Flask(__name__)
 
-def bookTableLookup(bookid):
-    return db["books"].find_one({"bookid":bookid})
+def bookTableLookup(bookId):
+    return db["books"].find_one({"bookid":bookId})
 
 @app.route("/getcomments", methods=["GET"])
 def getcomments():
-    bookid = int(request.args.get("bookid", 1))
+    bookId = int(request.args.get("bookId", 1))
     paragraphStartIndex = int(request.args.get("paragraphStartIndex", None))
-    elementStartIndex = int(request.args.get("elementStartIndex", None))
-    charStartIndex = int(request.args.get("charStartIndex", None))
     paragraphEndIndex = int(request.args.get("paragraphEndIndex", None))
+    elementStartIndex = int(request.args.get("elementStartIndex", None))
     elementEndIndex = int(request.args.get("elementEndIndex", None))
+    charStartIndex = int(request.args.get("charStartIndex", None))
     charEndIndex = int(request.args.get("charEndIndex", None))
     if not paragraphStartIndex or not elementStartIndex or not charStartIndex:
         return dumps({"error": "incorrect starting page indexing values"})
     if not paragraphEndIndex or not elementEndIndex or not charEndIndex:
         return dumps({"error": "incorrect ending page indexing values"})
 
-    collection = bookTableLookup(bookid)
+    collection = bookTableLookup(bookId)
     if not collection:
         return dumps({"error": "no such book"})
     collection = collection["collection"]
 
     query = {
-        "$and" : [{"paragraphStartIndex": {"$lte": paragraphEndIndex}},
-                {"paragraphEndIndex": {"$gte": paragraphStartIndex}}],
-        "$and" : [{"elementStartIndex": {"$lte": elementEndIndex}},
-                {"elementEndIndex": {"$gte": elementStartIndex}}],
-        "$and" : [{"charStartIndex": {"$lte": charEndIndex}},
-                {"charEndIndex": {"$gte": charStartIndex}}]                 
+        "paragraphStartIndex": {"$lte": paragraphEndIndex},
+        "paragraphEndIndex": {"$gte": paragraphStartIndex},
+        "elementStartIndex": {"$lte": elementEndIndex},
+        "elementEndIndex": {"$gte": elementStartIndex},
+        "charStartIndex": {"$lte": charEndIndex},
+        "charEndIndex": {"$gte": charStartIndex}
     }
     results = [x for x in db[collection].find(query)]
+
     return dumps(results)
 
 
